38c946289297257c3d44a631eec97e1c048ee0d4
Ngoc Anh Nguyen <ngoc.nguyen@rdf.nuernberg.de>
2025-06-05 11:26:32 +0200

    constants.py hinzugefügt

---
+++ constants.py
@@ -0,0 +1,13 @@
+TYPE_MAP = {
+    "Pflanze": 1, "Feuer": 2, "Wasser": 3, "Elektro": 4, "Psycho": 5,
+    "Kampf": 6, "Finsternis": 7, "Metall": 8, "Fee": 9, "Drache": 10, "Farblos": 11
+}
+
+RARITY_MAP = {
+    "Common": 1, "Uncommon": 2, "Rare": 3, "Double Rare": 4,
+    "Ultra Rare": 5, "Art Rare": 6, "Special Art Rare": 7, "Secret Rare": 8
+}
+
+PACK_MAP = {
+    "Reisegefährten": 1, "Welten im Wandel": 2
+}
