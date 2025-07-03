d3d63d2807c552763a8287c4572987ba42a0fda7
Ngoc Anh Nguyen <ngoc.nguyen@rdf.nuernberg.de>
2025-07-03 11:02:05 +0200

    Alle Karten Ansicht

---
+++ WindowsForms/WindowsForms/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WindowsForms
+{
+    partial class Pokebook
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanelCards = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanelCards
+            // 
+            this.flowLayoutPanelCards.AutoScroll = true;
+            this.flowLayoutPanelCards.Dock = System.Windows.Forms.DockStyle.Right;
+            this.flowLayoutPanelCards.Location = new System.Drawing.Point(308, 0);
+            this.flowLayoutPanelCards.Name = "flowLayoutPanelCards";
+            this.flowLayoutPanelCards.Size = new System.Drawing.Size(914, 772);
+            this.flowLayoutPanelCards.TabIndex = 0;
+            // 
+            // Pokebook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1222, 772);
+            this.Controls.Add(this.flowLayoutPanelCards);
+            this.Name = "Pokebook";
+            this.Text = "Pokebook";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelCards;
+    }
+}
+
---
+++ WindowsForms/WindowsForms/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsForms
+{
+    public partial class Pokebook : Form
+    {
+
+        public Pokebook()
+        {
+            InitializeComponent();
+            this.Load += Pokebook_Load;
+        }
+
+        private void Pokebook_Load(object sender, EventArgs e)
+        {
+            ShowAllCardImages();
+        }
+
+        private string imagesFolderPath = @"C:\Users\Ngoc Anh\source\repos\pokebook\assets\Karten_Bilder"; 
+
+        private void ShowAllCardImages()
+        {
+            DataTable cards = GetAllCardImageNamesFromDB();
+
+            flowLayoutPanelCards.Controls.Clear();
+
+            foreach (DataRow row in cards.Rows)
+            {
+                string imageName = row["Bildname"].ToString();
+
+                PictureBox pic = new PictureBox();
+                pic.Width = 140;
+                pic.Height = 190;
+                pic.SizeMode = PictureBoxSizeMode.Zoom;
+                pic.Margin = new Padding(5);
+
+                string imgPath = System.IO.Path.Combine(imagesFolderPath, imageName);
+                if (System.IO.File.Exists(imgPath))
+                {
+                    using (var temp = Image.FromFile(imgPath))
+                    {
+                        pic.Image = new Bitmap(temp);
+                    }
+                }
+
+                flowLayoutPanelCards.Controls.Add(pic);
+            }
+        }
+
+        private DataTable GetAllCardImageNamesFromDB()
+        {
+            string connStr = "server=127.0.0.1;database=team02;uid=na_ng;pwd=botanical;";
+            string query = "SELECT DISTINCT Bildname FROM karte ORDER BY Bildname ASC";
+
+            DataTable dt = new DataTable();
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    conn.Open();
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                    {
+                        adapter.Fill(dt);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("DB Fehler: " + ex.Message);
+            }
+
+            return dt;
+        }
+
+
+    }
+
+}
